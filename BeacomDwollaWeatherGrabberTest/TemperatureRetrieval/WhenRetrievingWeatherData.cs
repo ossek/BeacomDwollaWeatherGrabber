b818f8ff7465be0d4ab972fc76e10cc2afa974a5
ossek <ossek@things.com>
2018-03-18 16:19:18 -0500

    stubbing out some tests that fail

--- BeacomDwollaWeatherGrabberTest/TemperatureRetrieval/WhenRetrievingWeatherData.cs
+++ BeacomDwollaWeatherGrabberTest/TemperatureRetrieval/WhenRetrievingWeatherData.cs
@@ -12,12 +12,34 @@ namespace BeacomDwollaWeatherGrabberTest
         }
 
         [Fact]
-        public void WhenCityNameIsBad()
+        public void WhenTemperatureRetrievableThenReturnTemperature()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void WhenConnectionNotAvailableThenThrowException()
+        {
+            Assert.False(true);
+        }
+
+        
+    }
+
+    public class WhenCheckingCityName
+    {
+        [Fact]
+        public void WhenCityNameNotFoundBadThenReturnFalse()
         {
             var badCityService = new Mock<ITemperatureService>().Object;
             var badCity = "Cleveland";
             var isCityValid = badCityService.CityIsValid(badCity);
             Assert.False(isCityValid);
+        }
+
+        [Fact]
+        public void WhenCityNameFoundReturnTrue(){
+            Assert.False(true);
         }
     }
 }
